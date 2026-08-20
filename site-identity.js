d63349978391a67577c4d39f4a60ddaed8f65649
root <root@ops.simhaonline.com>
2026-08-20 23:52:53 +0300

    Remove PAT prefix from public labels

--- site-identity.js
+++ site-identity.js
@@ -5,7 +5,7 @@
     const nodes = [];
     while (walker.nextNode()) nodes.push(walker.currentNode);
     nodes.forEach(node => {
-      node.nodeValue = node.nodeValue.replaceAll('hello@predict-a-trade.com', 'predictatrade@gmail.com').replaceAll('Simha Online', 'Simha FinTech');
+      node.nodeValue = node.nodeValue.replaceAll('hello@predict-a-trade.com', 'predictatrade@gmail.com').replaceAll('Simha Online', 'Simha FinTech').replaceAll('PAT / ', '');
     });
     document.querySelectorAll('[href="https://simhaonline.com"]').forEach(link => { link.href = 'https://predictatrade.com/'; });
     document.querySelectorAll('.module-strip').forEach(node => node.remove());
